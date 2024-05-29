565f01d9b7738feae89d764d8e1dd5b862616e8e
aishyuu <ivanmatias1331@gmail.com>
2024-05-29 05:47:29 -0700

    Created main menu skeleton.

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,5 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-string response = Console.ReadLine();
+﻿Console.WriteLine("Please type your name");
 
-Console.WriteLine(response);
+var name = Console.ReadLine();
+var date = DateTime.UtcNow;
+
+Console.WriteLine("-----------------------------------------------------------");
+Console.WriteLine($"Hello {name.ToUpper()}. It's {date}. This is your math's game. That's great that you're working on improving yourself\n");
+Console.WriteLine($@"What are ya gonna play? You got:
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program");
+Console.WriteLine("-----------------------------------------------------------");
+
+var gameSelected = Console.ReadLine();
+
+switch(gameSelected.ToLower().Trim())
+{
+    case "a":
+        Console.WriteLine("Addition Selected");
+        break;
+
+    case "s":
+        Console.WriteLine("Subtraction Selected");
+        break;
+
+    case "m":
+        Console.WriteLine("Multiplication Selected");
+        break;
+
+    case "d":
+        Console.WriteLine("Multiplication Selected");
+        break;
+
+    default:
+        Console.WriteLine("Invalid Input");
+        break;
+}
